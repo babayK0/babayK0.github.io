bed12874121f3318ee4f4ab7b3a88918519d2af9
Bohdan Kovalchuk <33497965+babayK0@users.noreply.github.com>
2019-01-02 23:43:49 +0200

    rewrite code

--- a-tiny-JS-world/index.js
+++ a-tiny-JS-world/index.js
@@ -7,72 +7,45 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-const dog = {
-   species: 'dog',
-   gender: 'male',
-   name: 'Wolf',
-   legs: '4',
-   hands: '0',
-   saying: 'arf-aarf!',
-   friends: undefined
-};
-const cat = {
-   species: 'cat',
-   gender: 'female',
-   name: 'Serafina',
-   legs: '4',
-   hands: '0',
-   saying: 'meeeow!',
-   friends: undefined   
-};
-const woman = {
-   species: 'human',
-   gender: 'female',
-   name: 'Oprah',
-   legs: '2',
-   hands: '2',
-   saying: 'Failure is another steppingstone to greatness.',
-   friends: undefined
-};
-const man = {
-   species: 'human',
-   gender: 'male',
-   name: 'John',
-   legs: '2',
-   hands: '2',
-   saying: 'Your well-being depends on your own decisions.',
-   friends: undefined
-};
-const catwoman= {
-   species: woman.species,
-   gender: woman.gender,
-   name: 'Vi',
-   legs: '2',
-   hands: '2',
-   saying: cat.saying,
-   friends: undefined
-};
+class Organism{
+   constructor(species,gender,name,legs,hands,saying,friends){
+   this.species=species;
+   this.gender=gender;
+   this.name=name;
+   this.legs=legs;
+   this.hands=hands;
+   this.saying=saying;
+   this.friends=undefined;
+   }
+   get Info(){
+      return `I am ${this.species}-${this.gender}. My name is ${this.name}.I have ${this.legs} legs and ${this.hands} hands.`
+      +(this.friends==undefined ? '...oh, i haven\'t friends' : `My friends are ${this.friends}`)
+      +`.My favorite phrase is: <strong>${this.saying}</strong>`;
+   }
+}
+
+let dog = new Organism('dog','male','Wolf','4','0','arf-aarf!');
+let cat = new Organism('cat','female','Serafina','4','0','meeeow!');
+let woman = new Organism('human','female','Oprah','2','2','Failure is another steppingstone to greatness.');
+let man = new Organism('human','male','John','2','2','Your well-being depends on your own decisions.');
+let catwoman = new Organism(woman.species+cat.species,woman.gender,'Vi','2','2',cat.saying);
+
 dog.friends = `${cat.name} and ${man.name}`;
 cat.friends = `${dog.name} and ${woman.name}`;
 woman.friends = `${man.name} and ${cat.name}`;
 man.friends = `${woman.name} and ${dog.name}`;
 catwoman.friends = `${woman.name} and ${woman.friends}`
-
-function creatingOrganism(obj){
-      return `I am ${obj.species}-${obj.gender}. My name is ${obj.name}.I have ${obj.legs} legs and ${obj.hands} hands.`+(obj.friends==undefined ? '...oh, i haven\'t friends' : `My friends are ${obj.friends}`)+`.My favorite phrase is: <strong>${obj.saying}</strong>`;
-}
-
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
    Message can contain HTML markup. You may also tweak index.html and/or styles.css.
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.*/
-print(creatingOrganism(dog));
-print(creatingOrganism(cat));
-print(creatingOrganism(woman));
-print(creatingOrganism(man));
-print(creatingOrganism(catwoman));
+print(dog.Info);
+print(cat.Info);
+print(woman.Info);
+print(man.Info);
+print(catwoman.Info);
 /* Print examples:
    print('ABC');
    print('<strong>ABC</strong>');
